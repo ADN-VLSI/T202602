dfdd53619aec550a1017fd35e9210bef070ed6cf
Foez Ahmed <foez.official@gmail.com>
2026-03-16 11:43:18 +0600

    Refactor ci_if interface: standardize signal names and add control channel methods

--- interface/ci_if.sv
+++ interface/ci_if.sv
@@ -6,173 +6,210 @@ interface ci_if #(
     input logic clk_i     // clock
 );
 
+  //////////////////////////////////////////////////////////////////////////////////////////////////
+  // SIGNALS
+  //////////////////////////////////////////////////////////////////////////////////////////////////
+
   // Control Channel
-  logic [  ADDR_WIDTH-1:0] c_addr_i;
-  logic                    c_wenable_i;
-  logic                    c_valid_i;
-  logic                    c_ready_o;
+  logic [  ADDR_WIDTH-1:0] c_addr;
+  logic                    c_wenable;
+  logic                    c_valid;
+  logic                    c_ready;
 
   // Write Data Channel
-  logic [  DATA_WIDTH-1:0] w_data_i;
-  logic [DATA_WIDTH/8-1:0] w_strb_i;
-  logic                    w_valid_i;
-  logic                    w_ready_o;
+  logic [  DATA_WIDTH-1:0] w_data;
+  logic [DATA_WIDTH/8-1:0] w_strb;
+  logic                    w_valid;
+  logic                    w_ready;
 
   // Read Data Channel
-  logic [  DATA_WIDTH-1:0] r_data_o;
-  logic                    r_valid_o;
-  logic                    r_ready_i;
+  logic [  DATA_WIDTH-1:0] r_data;
+  logic                    r_valid;
+  logic                    r_ready;
 
   // Response Channel
-  logic                    b_slverr_o;
-  logic                    b_valid_o;
-  logic                    b_ready_i;
+  logic                    b_slverr;
+  logic                    b_valid;
+  logic                    b_ready;
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////
+  // VARIABLES
+  //////////////////////////////////////////////////////////////////////////////////////////////////
+
+  bit                      is_edge_aligned = '0;
+
+  always @(posedge clk_i) begin
+    is_edge_aligned <= '1;
+    #1ps;
+    is_edge_aligned <= '0;
+  end
 
   //////////////////////////////////////////////////////////////////////////////////////////////////
   // METHODS
   //////////////////////////////////////////////////////////////////////////////////////////////////
 
-  ///////////////// CONTROL CHANNEL METHODS /////////////////
-  // TODO: Alif: Add control channel methods if needed
-  // CONTROL CHANNEL METHODS
-
-  semaphore ctrl_sema = new(1);
   // Reset control channel
-  task automatic ctrl_reset();
-    c_addr_i    <= '0;
-    c_wenable_i <= 1'b0;
-    c_valid_i   <= 1'b0;
+  task automatic master_reset();
+    c_addr    <= '0;
+    c_wenable <= '0;
+    c_valid   <= '0;
+    w_data    <= '0;
+    w_strb    <= '0;
+    w_valid   <= '0;
+    r_ready   <= '0;
+    b_ready   <= '0;
   endtask
 
-
-  // Send control request (address phase)
-  task automatic ctrl_request(
-      input  logic [ADDR_WIDTH-1:0] addr,
-      input  logic                  wenable
-  );
-    ctrl_sema.get(1);
-
-    // Align to clock edge
-    @(posedge clk_i);
-
-    c_addr_i    <= addr;
-    c_wenable_i <= wenable;
-    c_valid_i   <= 1'b1;
-
-    // Wait for ready (clock synchronous)
-    while (!c_ready_o)
-      @(posedge clk_i);
-
-    // Deassert valid after handshake
-    @(posedge clk_i);
-    c_valid_i <= 1'b0;
-
-    ctrl_sema.put(1);
+  // Reset control channel
+  task automatic slave_reset();
+    c_ready   <= '0;
+    w_ready   <= '0;
+    r_data    <= '0;
+    r_valid   <= '0;
+    b_slverr  <= '0;
+    b_valid   <= '0;
   endtask
 
+  ////////////////////////////////////////////////
+  // CONTROL CHANNEL METHODS
+  ////////////////////////////////////////////////
 
-  // Wait until control handshake completes
-  task automatic ctrl_wait_handshake();
-    while (!(c_valid_i && c_ready_o))
-      @(posedge clk_i);
+  // Master: Send control request (address phase)
+  semaphore ctrl_send_sema = new(1);
+  task automatic send_c(input logic [ADDR_WIDTH-1:0] addr, input logic wenable);
+    ctrl_send_sema.get(1);
+    if (arst_ni) wait (is_edge_aligned);
+    c_addr    <= addr;
+    c_wenable <= wenable;
+    c_valid   <= 1'b1;
+    do @(posedge clk_i); while (!c_ready && arst_ni);
+    c_valid <= 1'b0;
+    ctrl_send_sema.put(1);
   endtask
 
-
-
-
-
-
-
-  
-  ///////////////// WRITE DATA CHANNEL METHODS /////////////////
-  // TODO: Ratul: Add write data channel methods if needed
-
-  task automatic send_w(input logic [DATA_WIDTH-1:0] data,
-                            input logic [DATA_WIDTH/8-1:0] strb);
-    w_data_i <= data;
-    w_strb_i <= strb;
-    w_valid_i <= 1'b1;
-    do @(posedge clk_i); while (!w_ready_o);
-    w_valid_i <= 1'b0;
+  // Slave: Receive control request (address phase)
+  semaphore ctrl_recv_sema = new(1);
+  task automatic recv_c(output logic [ADDR_WIDTH-1:0] addr, output logic wenable);
+    ctrl_recv_sema.get(1);
+    if (arst_ni) wait (is_edge_aligned);
+    c_ready <= 1'b1;
+    do @(posedge clk_i); while (!c_valid && arst_ni);
+    addr = c_addr;
+    wenable = c_wenable;
+    c_ready <= 1'b0;
+    ctrl_recv_sema.put(1);
   endtask
 
-  task automatic recv_w(output logic [DATA_WIDTH-1:0] data,
-                          output logic [DATA_WIDTH/8-1:0] strb);
-    w_ready_o <= 1'b1;
-    do @(posedge clk_i); while (!w_valid_i);
-    data = w_data_i;
-    strb = w_strb_i;
-    w_ready_o <= 1'b0;
+  // Monitor: Look at control request
+  task automatic look_c(output logic [ADDR_WIDTH-1:0] addr, output logic wenable);
+    do @(posedge clk_i); while (!(c_valid && c_ready && arst_ni));
+    addr = c_addr;
+    wenable = c_wenable;
   endtask
 
-  task automatic look_w(output logic [DATA_WIDTH-1:0] data,
-                          output logic [DATA_WIDTH/8-1:0] strb);
-    do @(posedge clk_i); while (!(w_valid_i && w_ready_o));
-    data = w_data_i;
-    strb = w_strb_i;
+  ////////////////////////////////////////////////
+  // WRITE DATA CHANNEL METHODS
+  ////////////////////////////////////////////////
+
+  // Master: Send write data (data phase)
+  semaphore w_send_sema = new(1);
+  task automatic send_w(input logic [DATA_WIDTH-1:0] data, input logic [DATA_WIDTH/8-1:0] strb);
+    w_send_sema.get(1);
+    if (arst_ni) wait (is_edge_aligned);
+    w_data  <= data;
+    w_strb  <= strb;
+    w_valid <= 1'b1;
+    do @(posedge clk_i); while (!w_ready && arst_ni);
+    w_valid <= 1'b0;
+    w_send_sema.put(1);
   endtask
 
-  task automatic reset_w();
-    w_data_i  <= '0;
-    w_strb_i  <= '0;
-    w_valid_i <= 1'b0;
+  // Slave: Receive write data (data phase)
+  semaphore w_recv_sema = new(1);
+  task automatic recv_w(output logic [DATA_WIDTH-1:0] data, output logic [DATA_WIDTH/8-1:0] strb);
+    w_recv_sema.get(1);
+    if (arst_ni) wait (is_edge_aligned);
+    w_ready <= 1'b1;
+    do @(posedge clk_i); while (!w_valid && arst_ni);
+    data = w_data;
+    strb = w_strb;
+    w_ready <= 1'b0;
+    w_recv_sema.put(1);
   endtask
 
-  ///////////////// READ DATA CHANNEL METHODS /////////////////
-  // TODO: Sabbir: Add read data channel methods if needed
-    // Slave: Send read data
-  task automatic send_read_data(
-      input logic [DATA_WIDTH-1:0] data
-  );
-    @(posedge clk_i);
-    r_data_o  <= data;
-    r_valid_o <= 1'b1;
-
-do @(posedge clk_i);
-while (!r_ready_i)
-    
-    r_valid_o <= 1'b0;
+  // Monitor: Look at write data
+  task automatic look_w(output logic [DATA_WIDTH-1:0] data, output logic [DATA_WIDTH/8-1:0] strb);
+    do @(posedge clk_i); while (!(w_valid && w_ready && arst_ni));
+    data = w_data;
+    strb = w_strb;
   endtask
 
+  ////////////////////////////////////////////////
+  // READ DATA CHANNEL METHODS
+  ////////////////////////////////////////////////
+
+  // Slave: Send read data (data phase)
+  semaphore r_send_sema = new(1);
+  task automatic send_r(input logic [DATA_WIDTH-1:0] data);
+    r_send_sema.get(1);
+    if (arst_ni) wait (is_edge_aligned);
+    r_data  <= data;
+    r_valid <= 1'b1;
+    do @(posedge clk_i); while (!r_ready && arst_ni);
+    r_valid <= 1'b0;
+    r_send_sema.put(1);
+  endtask
 
   // Master: Receive read data
-  task automatic get_read_data(
-      output logic [DATA_WIDTH-1:0] data
-  );
-    r_ready_i <= 1'b1;
-    wait (r_valid_o);
-    data = r_data_o;
-    @(posedge clk_i);
-    r_ready_i <= 1'b0;
+  semaphore r_recv_sema = new(1);
+  task automatic recv_r(output logic [DATA_WIDTH-1:0] data);
+    r_recv_sema.get(1);
+    if (arst_ni) wait (is_edge_aligned);
+    r_ready <= 1'b1;
+    do @(posedge clk_i); while (!r_valid && arst_ni);
+    data = r_data;
+    r_ready <= 1'b0;
+    r_recv_sema.put(1);
   endtask
 
-
-
-  ///////////////// RESPONSE CHANNEL METHODS /////////////////
-  // TODO: Sabbir: Add response channel methods if needed
-    // Slave: Send response
-  task automatic send_response(
-      input logic slverr
-  );
-    @(posedge clk_i);
-    b_slverr_o <= slverr;
-    b_valid_o  <= 1'b1;
-
-    wait (b_ready_i);
-    @(posedge clk_i);
-    b_valid_o <= 1'b0;
+  // Monitor: Look at read data
+  task automatic look_r(output logic [DATA_WIDTH-1:0] data);
+    do @(posedge clk_i); while (!(r_valid && r_ready && arst_ni));
+    data = r_data;
   endtask
 
+  ////////////////////////////////////////////////
+  // RESPONSE CHANNEL METHODS
+  ////////////////////////////////////////////////
+
+  // Slave: Send response
+  semaphore b_send_sema = new(1);
+  task automatic send_b(input logic slverr);
+    b_send_sema.get(1);
+    if (arst_ni) wait (is_edge_aligned);
+    b_slverr <= slverr;
+    b_valid  <= 1'b1;
+    do @(posedge clk_i); while (!b_ready && arst_ni);
+    b_valid <= 1'b0;
+    b_send_sema.put(1);
+  endtask
 
   // Master: Get response
-  task automatic get_response(
-      output logic slverr
-  );
-    b_ready_i <= 1'b1;
-    wait (b_valid_o);
-    slverr = b_slverr_o;
-    @(posedge clk_i);
-    b_ready_i <= 1'b0;
+  semaphore b_recv_sema = new(1);
+  task automatic recv_b(output logic slverr);
+    b_recv_sema.get(1);
+    if (arst_ni) wait (is_edge_aligned);
+    b_ready <= 1'b1;
+    do @(posedge clk_i); while (!b_valid && arst_ni);
+    slverr = b_slverr;
+    b_ready <= 1'b0;
+    b_recv_sema.put(1);
+  endtask
+
+  // Monitor: Look at response
+  task automatic look_b(output logic slverr);
+    do @(posedge clk_i); while (!(b_valid && b_ready && arst_ni));
+    slverr = b_slverr;
   endtask
 
 endinterface
