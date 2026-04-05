87089eea914b6798b4cddcf5ab7427ba29d4eef0
Sabbir <sabbirone939@gmail.com>
2026-04-05 20:47:35 +0600

    bin2gray verilog and tb done

---
+++ practice/bin2gray_tb.v
@@ -0,0 +1,18 @@
+module bin2gray_tb;
+  parameter n=4;
+  
+  reg [n-1:0] bin;
+  wire [n-1:0] gray_op;
+ 
+  bin2gray dut(bin,gray_op);
+  initial begin
+    //$monitor("bin=%b,gray_op=%b",bin,gray_op);
+    bin=4'b1101;
+    #5;
+    bin=4'b1010;
+    
+   #5;
+    $finish;
+  end
+endmodule
+  
