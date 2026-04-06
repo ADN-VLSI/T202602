18d655aa5c1e128e8d7e0f9378badc9b57847ee8
Sabbir <sabbirone939@gmail.com>
2026-04-06 12:38:38 +0600

    Applied parameterized design

--- practice/dual_flop_sync.sv
+++ practice/dual_flop_sync.sv
@@ -1,22 +1,22 @@
-module dual_flop_sync (
-    input  logic clk,        // Destination clock domain
-    input  logic rst_n,      // Active-low reset
-    input  logic async_in,   // Asynchronous input signal
-    output logic sync_out    // Synchronized output
+module sync_2ff #(parameter WIDTH = 8)(
+    input wire clk,                // Destination clock domain
+    input wire rst_n,              // Active-low reset
+    input wire [WIDTH-1:0] async_in, // Asynchronous input signal
+    output wire [WIDTH-1:0] sync_out // Synchronized output
 );
 
-    logic sync_ff1, sync_ff2;
+    reg [WIDTH-1:0] sync_ff1, sync_ff2; // 2 FF registers for synchronization
 
-    always_ff @(posedge clk or negedge rst_n) begin
+    always @(posedge clk ) begin
         if (!rst_n) begin
-            sync_ff1 <= 1'b0;
-            sync_ff2 <= 1'b0;
+            sync_ff1 <= {WIDTH{1'b0}};
+            sync_ff2 <= {WIDTH{1'b0}};
         end else begin
             sync_ff1 <= async_in;   // First stage
             sync_ff2 <= sync_ff1;   // Second stage
         end
     end
 
-    assign sync_out = sync_ff2;
+    assign sync_out = sync_ff2; // Synchronized output
 
 endmodule
