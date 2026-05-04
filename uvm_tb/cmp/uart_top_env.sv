3d8dc46c50b4ad30d8cf4ae111e6bce47505d5d7
Foez Ahmed <foez.official@gmail.com>
2026-05-04 12:39:48 +0600

    Refactor UART scoreboard and environment: add analysis ports, update class names, and improve structure

--- uvm_tb/cmp/uart_top_env.sv
+++ uvm_tb/cmp/uart_top_env.sv
@@ -3,14 +3,16 @@
 
 `include "cmp/apb_agent.sv"
 `include "cmp/uart_agent.sv"
+`include "cmp/uart_top_scbd.sv"
 
 class uart_top_env extends uvm_env;
 
   // UVM component utilities for factory registration
   `uvm_component_utils(uart_top_env)
 
-  apb_agent  apb;
-  uart_agent uart;
+  apb_agent     apb;
+  uart_agent    uart;
+  uart_top_scbd scbd;
 
   // Constructor for the environment
   function new(string name = "uart_top_env", uvm_component parent = null);
@@ -22,6 +24,13 @@ class uart_top_env extends uvm_env;
     super.build_phase(phase);
     apb  = apb_agent::type_id::create("apb", this);
     uart = uart_agent::type_id::create("uart", this);
+    scbd = uart_top_scbd::type_id::create("scbd", this);
+  endfunction
+
+  function void connect_phase(uvm_phase phase);
+    super.connect_phase(phase);
+    apb.ap.connect(scbd.m_analysis_imp_apb);
+    uart.ap.connect(scbd.m_analysis_imp_uart);
   endfunction
 
 endclass
